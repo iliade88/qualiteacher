fa21e8a00c5f59f1d494322478ff73a5a356c3ae
Cristobal Jesus <cris_j_g@hotmail.com>
2017-05-05 17:02:08 +0200

    Añadidas universidades a la db de mongo. Ahora a la vista de registro se le pasa el array de universidades para poder seleccionarla. Preparado crear usuario para recibir el id de universidad a la que pertenece

--- qualiteacher/app.js
+++ qualiteacher/app.js
@@ -4,9 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/qualiteacher');
+require('./models/Universidades');
+require('./models/Usuarios');
+require('./models/Profesores');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var usuarios = require('./routes/usuarios');
+var universidades = require('./routes/universidades');
 
 var app = express();
 
@@ -23,7 +30,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/usuarios', usuarios);
+app.use('/universidades', universidades);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- qualiteacher/routes/index.js
+++ qualiteacher/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Qualiteacher' });
 });
 
+/* GET - Página registro. */
+router.get('/registro', function(req, res, next) {
+
+	var universidades_select;
+	universidades.find(function(err, universidades){
+		if (err) console.log(err)
+
+		universidades_select = universidades;
+	});
+
+	res.render('registro', {title : 'Qualiteacher | Registro', universidades : universidades_select});
+});
+
 module.exports = router;
