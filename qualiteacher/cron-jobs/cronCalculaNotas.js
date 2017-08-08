9b576d53f3b375a7b1890dbf546f7162f7b30408
Cristobal Jesus <cris_j_g@hotmail.com>
2017-08-08 11:57:33 +0200

    Modificada vista de carrera para usar el nuevo sistema de votos. Deshabilitado el cron-job ya que se está barajando si es necesario realmente

--- qualiteacher/cron-jobs/cronCalculaNotas.js
+++ qualiteacher/cron-jobs/cronCalculaNotas.js
@@ -8,9 +8,9 @@ exports.lanzaScheduler = function()
 	/**
 	 * Cron cada día a las 23:00
 	 */
-	var task = cron.schedule('0 0 23 * * *', function(){
+	/*var task = cron.schedule('0 0 23 * * *', function(){
 		UniversidadesController.calculaNotas();
 	});
 
-	task.start();
+	task.start();*/
 }
